200016ac6aad948c633e3bc469776be42c8a3b1d
atenagroupweb <118869753+atenagroupweb@users.noreply.github.com>
2022-11-24 05:18:06 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,28 +2,15 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+// Collapse responsive navbar when hamburger is visible
+    let hamburger_menu = document.querySelector('.icon-hamburger');
+    hamburger_menu.addEventListener("click", function() {
+    document.body.classList.toggle('menu-open');
     });
 
-});
+// Collapse responsive navbar on click of navbar item menu 
+    let item_hamburger = document.querySelector('.navbar-menu');
+    item_hamburger.addEventListener("click", function() {
+    document.body.classList.toggle('menu-open');
+    });
