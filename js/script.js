6862ad355b28afa5d16d5d36f9afb126c2d0b1fb
atenagroupweb <118869753+atenagroupweb@users.noreply.github.com>
2022-11-29 03:49:55 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,3 +14,28 @@
     item_hamburger.addEventListener("click", function() {
     document.body.classList.toggle('menu-open');
     });
+
+
+// Animazioni
+
+   // elements
+   var elements_to_watch = document.querySelectorAll('.watch');
+
+   // callback 
+   var callback = function(items){
+     items.forEach((item) => {
+       if(item.isIntersecting){
+         item.target.classList.add("anim");
+       } else{
+         item.target.classList.remove("anim");
+       }
+     });
+   }
+
+   // observer
+   var observer = new IntersectionObserver(callback, { threshold: 0.7 } );
+
+   // apply
+   elements_to_watch.forEach((element) => {
+     observer.observe(element); 
+   });
